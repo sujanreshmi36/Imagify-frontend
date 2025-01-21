26a76ccb47b23b17d19d825c109a9a6baa9a1bc3
sujanreshmi36 <sujan.reshmi.7@gmail.com>
2025-01-21 16:53:28 +0545

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import BuyCredit from "./pages/BuyCredit";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AppContextProvider from "./context/AppContext";
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buy" element={<BuyCredit />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/BuyCredit.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BuyCredit = () => {
+  return (
+    <div>
+      <h1>Buy Credit</h1>
+    </div>
+  );
+};
+
+export default BuyCredit;
