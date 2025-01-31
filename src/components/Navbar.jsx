4521a79c08eddc3cf3f8d4ce39bcf9738e0144d6
sujanreshmi36 <sujan.reshmi.7@gmail.com>
2025-01-31 13:34:07 +0545

    pricing is added in login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,9 @@ const Navbar = () => {
       <div>
         {user ? (
           <div className="flex items-center gap-2 sm:gap-3">
+            <p onClick={() => navigate("/buy")} className="cursor-pointer">
+              Pricing
+            </p>
             <button className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700">
               <img className="w-5" src={assets.credit_star} alt="" />
               <p className="text-xs sm:text-sm font-medium text-gray-600">
